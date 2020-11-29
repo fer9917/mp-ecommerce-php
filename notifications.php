fd86a5aa0109a3e7e68080b70bfffe41c4521ee4
Fer De La Cruz <ferdelacruz9917@gmail.com>
2020-11-29 13:26:18 -0600

    Log

--- notifications.php
+++ notifications.php
@@ -11,6 +11,10 @@ echo "Notification success";
 $message = "JSON: ".json_encode($_REQUEST)."<br>";
 $message .= '<pre>'.print_r($_REQUEST, true).'</pre>';
 
+file_put_contents('info.log', "\n".date("Y-m-d H:i:s")."\n", FILE_APPEND);
+file_put_contents('info.log', "\n".json_encode($_REQUEST)."\n", FILE_APPEND);
+file_put_contents('info.log', print_r($_REQUEST, true), FILE_APPEND);
+
 $mail->send([
     'to' => 'fer@gestionmx.net',
     'tittle' => 'Notifications Mercado',
